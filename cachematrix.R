4e6868ef56379b3c87c3c8b00c01a0bae906d92f
sadiqiqbal <malar.poo@gmx.de>
2015-09-21 20:51:42 +0200

    my solution to the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit to 
+## caching the inverse of a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
+## The below function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) { # start makeCacheMatrix
+  # stores the cached value
+  # initialize to NULL
+  inv <- NULL
+  set <- function(y) { # to create matrix in working environment
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x # get the value of the matrix
+  setinverse <- function(inverse) inv <<- inverse # invert the matrix and store in cache
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) # return to the working environment
+}                                          # end makeCacheMatrix
 
-makeCacheMatrix <- function(x = matrix()) {
+## The below function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x=matrix(), ...) {  # start cacheSolve
+  inv <- x$getinverse() # seek the inverse of the matrix from cache
+  if(!is.null(inv)) { # gives matrix inverse from cache if it exists, otherwise create the matrix in working environment 
+    message("getting cached data.")
+    return(inv) # to display matrix
+  }                   
+  matrix <- x$get()
+  inv <- solve(matrix, ...)                # solve(matrix) returns the inverse
+  x$setinverse(inv)
+  inv
+}                                          # end cacheSolve
